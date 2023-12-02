832e297cf5cd3fe3745e2e7b4b3759f747fcd5bb
010binary <augustine32fg@gmail.com>
2023-12-01 18:43:43 -0800

    correction 1

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,8 +3,6 @@
 if __name__ == "__main__":
     a = 1
     b = 2
- 
     from add_0 import add
-
     result =  add(a, b)
-    print("{} + {} = {}".format(a, b, result))
+    print(f"{a} + {b} = {result}")
