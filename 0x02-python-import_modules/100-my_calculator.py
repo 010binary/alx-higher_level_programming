749f37cdb26e28766be8dd3b343d67b4e00e8067
010binary <augustine32fg@gmail.com>
2023-12-01 18:23:45 -0800

    Project 7

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+from calculator_1 import add, sub, mul, div
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: ./calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    operand1, operator, operand2 = int(sys.argv[1]), sys.argv[2], int(sys.argv[3])
+
+    result = None
+
+    if operator == '+':
+        result = add(operand1, operand2)
+    elif operator == '-':
+        result = sub(operand1, operand2)
+    elif operator == '*':
+        result = mul(operand1, operand2)
+    elif operator == '/':
+        result = div(operand1, operand2)
+    else:
+        print("Unknown operator. Available operators: +, -, *, and /")
+        sys.exit(1)
+
+    print(f"{operand1} {operator} {operand2} = {result}")
