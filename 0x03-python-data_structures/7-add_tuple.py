f036f4f9a13b4e56889314ca1ef9e6ca3ffcf178
010binary <augustine32fg@gmail.com>
2023-12-03 15:56:20 -0800

    Task 8

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    a = tuple_a[0] if len(tuple_a) > 0 else 0
+    b = tuple_a[1] if len(tuple_a) > 1 else 0
+    c = tuple_b[0] if len(tuple_b) > 0 else 0
+    d = tuple_b[1] if len(tuple_b) > 1 else 0
+
+    sum_first = a + c
+    sum_second = b + d
+
+    return (sum_first, sum_second)
