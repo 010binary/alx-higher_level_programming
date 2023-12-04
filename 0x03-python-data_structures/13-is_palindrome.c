5608ee047485dad2db115cfde31ce12edf450b28
010binary <augustine32fg@gmail.com>
2023-12-03 16:28:38 -0800

    Task 14

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,35 +1,77 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
+#include <stdlib.h>
 
+listint_t *reverse_listint(listint_t **head);
+int is_palindrome(listint_t **head);
 /**
- * is_palindrome - checks if a singly linked list is a palindrome
- * @head: pointer to pointer of the first node of the list
- * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ * reverse_listint - func reverses singly-linked listint_t list
+ * @head: head node pointer
+ * Return: list head pointer
  */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *node = *head, *next, *prev = NULL;
 
+	while (node)
+	{
+		next = node->next;
+		node->next = prev;
+		prev = node;
+		node = next;
+	}
+	*head = prev;
+	return (*head);
+}
+/**
+ * is_palindrome - func checks if singly linked list is a palindrome
+ * @head: head linked list pointer
+ * Return: if linked list is not a palindrome (0),
+ * if linked list is a palindrome (1)
+ */
 int is_palindrome(listint_t **head)
 {
-	listint_t *current = *head;
-	int arr[1000];
-	int i = 0, j;
+	listint_t *tmp, *rev, *mid;
+	size_t size = 0, i;
 
 	if (*head == NULL || (*head)->next == NULL)
+	{
 		return (1);
-
-	while (current != NULL)
-	{
-		arr[i] = current->n;
-		current = current->next;
-		i++;
 	}
 
-	i--;
-	for (j = 0; j < i; j++, i--)
+	tmp = *head;
+	while (tmp)
 	{
-		if (arr[j] != arr[i])
+		size++;
+		tmp = tmp->next;
+	}
+
+	tmp = *head;
+	for (i = 0; i < (size / 2) - 1; i++)
+	{
+		tmp = tmp->next;
+	}
+
+	if ((size % 2) == 0 && tmp->n != tmp->next->n)
+	{
+		return (0);
+	}
+
+	tmp = tmp->next->next;
+	rev = reverse_listint(&tmp);
+	mid = rev;
+
+	tmp = *head;
+	while (rev)
+	{
+		if (tmp->n != rev->n)
+		{
 			return (0);
+		}
+		tmp = tmp->next;
+		rev = rev->next;
 	}
+	reverse_listint(&mid);
 
 	return (1);
 }
