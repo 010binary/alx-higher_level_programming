eb4f94c924092f9a3c1acff9bd10b5933ee30fff
010binary <augustine32fg@gmail.com>
2023-12-18 17:26:28 -0800

    pushing it2

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,11 +1,12 @@
+#!/usr/bin/python3
+
 import sys
 
 def safe_print_integer_err(value):
-
-
     try:
-        print("{:d}".format(value))
-        return True
-    except Exception as e:
-        print(f"Exception: {e}", file=sys.stderr)
-        return False
+        if "{:d}".format(value):
+            print("{:d}".format(value))
+            return True
+    except ValueError:
+        sys.stderr.write("Exception: Unknown format code 'd' for object of type '{}'\n".format(type(value)))
+    return False
