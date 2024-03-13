861fe875e9f432705e4165839b6864710af00e8e
010binary <Augustine32fg@gmail.com>
2024-03-12 17:20:56 -0700

    semistandard 2

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -3,7 +3,7 @@
 const fileA = process.argv[2];
 const fileB = process.argv[3];
 const fileC = process.argv[4];
-const files = require('fs');
+const fs = require('fs');
 const textA = fs.readFileSync(fileA, 'utf8');
 const textB = fs.readFileSync(fileB, 'utf8');
-files.writeFileSync(fileC, textA + textB);
+fs.writeFileSync(fileC, textA + textB);
