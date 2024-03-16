f6931a1086cfac6f797d0cc719b64e4884320d7d
010binary <augustine32fg@gmail.com>
2024-03-16 11:16:50 -0700

    task 1

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,1 +1,27 @@
-#!/usr/bin/python3`
+#!/usr/bin/python3
+"""
+return all table values (table 'states')
+parameters given to script: username, password, database
+"""
+
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+
+    # connecting to the db
+    db = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3],)
+
+    # create cursor to exec queries using SQL
+    curs = db.cursor()
+    curs.execute("SELECT * FROM `states` ORDER BY `id` ASC")
+    [print(state) for state in curs.fetchall()]
+    curs.close()
+    db.close()
