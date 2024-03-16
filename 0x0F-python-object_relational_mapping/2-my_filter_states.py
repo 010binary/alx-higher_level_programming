ef864f45256765fde1ad1612dee4df0de326831a
010binary <augustine32fg@gmail.com>
2024-03-16 11:41:18 -0700

    task 1 querying my db for states with N

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+displays all values in the states table of hbtn_0e_0_usa
+where name matches the argument
+"""
+
+
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+
+    # Connecting to the db 
+    db = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3])
+
+    #Executing my query
+    cur = db.cursor()
