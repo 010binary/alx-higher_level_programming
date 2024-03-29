87f8ea31a9d9df723c9a8596c5abb0cdc43979dc
Augustine Chukwuemeka <99992586+010binary@users.noreply.github.com>
2024-03-29 03:07:25 +0100

    corrections

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """
-Script that fetches https://intranet.hbtn.io/status
+Script that fetches https://alx-intranet.hbtn.io/status
 this can be moded if need be 
 """
 import requests
 
-url = "https://intranet.hbtn.io/status"
+url = "https://alx-intranet.hbtn.io/status"
 
 if __name__ == '__main__':
     response = requests.get(url).text
