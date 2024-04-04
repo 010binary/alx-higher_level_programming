b677a2e0b435be27d697b30482b1d006c750f462
Augustine Chukwuemeka <99992586+010binary@users.noreply.github.com>
2024-04-04 02:05:04 +0100

    Update 100-status_code.sh

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a post request to a URL, and displays only the status code of the response.
+# script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
 curl -s -o /dev/null -w "%{http_code}" "$1"
