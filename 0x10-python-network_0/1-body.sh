c56a8f20d67c4533debd180866cc174a3f1a15fd
010binary <augustine32fg@gmail.com>
2024-04-03 17:43:48 -0700

    made the scripts 3 line wc -l

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # The size must be displayed in bytes and You have to use curl
-
 curl -s -L "$1"
--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # sends a post request to a URL, and displays only the status code of the response.
-
 curl -s -o /dev/null -w "%{http_code}" "$1"
